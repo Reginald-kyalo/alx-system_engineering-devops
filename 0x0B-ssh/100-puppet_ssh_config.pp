55a8d2205671fc704eb4275a5bac1f6648189dac
Reginald-kyalo <reginald.kyalo@gmail.com>
2023-11-28 15:48:52 -0800

    added changes

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,17 @@
+#configues ssh client to use private key in school file
+#and to refuse to authenticate using a password
 file { '/etc/ssh/ssh_config':
   ensure => present,
   owner  => 'root',
   group  => 'root',
   mode   => '0644',
   source => '/etc/ssh/ssh_config',
+}
 
 augeas { 'ssh_config':
   context => '/etc/ssh/ssh_config',
   changes => [
-    "set PasswordAuthentication no",
-    "set IdentityFile ~/.ssh/school",
+    'set PasswordAuthentication no',
+    'set IdentityFile ~/.ssh/school',
   ],
 }
