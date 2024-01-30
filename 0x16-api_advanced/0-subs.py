ef35cf349847a8a3c12c6131f86562a0cd335252
reginaldKyalo <111074460+Reginald-kyalo@users.noreply.github.com>
2024-01-31 00:59:27 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-   headers = {'User-Agent': 'xica369'}
+    headers = {'User-Agent': 'xica369'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
